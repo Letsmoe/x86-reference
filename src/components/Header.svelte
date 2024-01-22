eafff4a1cb108ff0083d48964601ac5637e5b1b1
Moritz Utcke <letsmoe1404@gmail.com>
2024-01-22 14:14:07 +0700

    Initial Commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ThemeController from "./ThemeController.svelte";
+
+	
+</script>
+
+<nav class="navbar bg-base-100 border-b">
+	<div class="max-w-screen-lg flex flex-row justify-between w-full mx-auto">
+		<a href="/" class="link link-hover text-xl">x86 Reference</a>
+		<ThemeController></ThemeController>
+	</div>
+</nav>
