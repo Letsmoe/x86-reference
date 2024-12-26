74001791905a477c1186857f5a038e3737a09b6b
Moritz Utcke <letsmoe1404@gmail.com>
2024-12-26 10:59:30 +0700

    Update build.bash

--- build.bash
+++ build.bash
@@ -6,5 +6,5 @@ bun run build
 # Restart the pm2 server
 pm2 stop meta-lang.com
 pm2 delete meta-lang.com
-pm2 start server.mjs --name meta-lang.com
+pm2 start --interpreter ~/.bun/bin/bun server.mjs --name meta-lang.com
 pm2 save
